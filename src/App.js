b92d7d63ca68928286f43f6122c98f6e92c2ca38
rupesh21CS19 <nikku090703@gmail.com>
2023-11-26 21:57:56 +0530

    d

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Projects from "./components/MyProjects/Projects";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/RupeshPF">
+      <BrowserRouter basename="/">
         <Routes>
-          <Route index path="/RupeshPF" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
+          <Route index path="RupeshPF" element={<Home />} />
+          <Route path="/RupeshPF/projects" element={<Projects />} />
         </Routes>
       </BrowserRouter>
     </>
