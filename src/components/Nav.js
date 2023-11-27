9d3adde41e290255cdcd9a73215b08a752f49b05
rupesh21CS19 <nikku090703@gmail.com>
2023-11-27 14:11:45 +0530

    d

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,23 @@
-import {  Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const Nav = () => {
   return (
     <div className="flex justify-between bg-black text-white  px-[2vw]  sm:px-[10vw] py-5 items-center sticky top-0 ">
-      <Link to="/RupeshPF" className="text-red-700 font-bold text-3xl">
+      <NavLink to="/RupeshPF" className="text-red-700 font-bold text-3xl">
         Rupesh.
-      </Link>
+      </NavLink>
       <div className="gap-4 flex">
-        <Link
+        <NavLink
           className="cursor-pointer transition ease-in-out delay-1  hover:-translate-y-1 hover:scale-110 duration-300 "
           to="/RupeshPF"
         >
           Home
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           className="cursor-pointer transition ease-in-out delay-1  hover:-translate-y-1 hover:scale-110 duration-300 "
           to="/projects"
         >
           Projects
-        </Link>
+        </NavLink>
       </div>
     </div>
   );
